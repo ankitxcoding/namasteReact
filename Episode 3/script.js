6baa561f73cd4c513b525035208c58e54945cdb4
ankitxcoding <ankitxcoding@gmail.com>
2023-11-10 10:32:05 +0530

    namasteReact Episode 3

--- Episode 3/script.js
+++ Episode 3/script.js
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading=<h1>Hello From JSX</h1>;
+const heading=(
+    <div id="subroot">
+        <h1>This is a h1 tag in subroot div</h1>
+    </div>
+);
+
+const Title=()=> {
+    return (
+    <div id="title">
+        <h1>This is a h1 tag in title div</h1>
+    </div>
+    )
+};
+
+const Header=()=> {
+    return (
+        <div id="header">
+            {heading}
+            <Title />
+            <h1>This is a h1 tag in header div</h1>
+        </div>
+    )
+}
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Header />);
