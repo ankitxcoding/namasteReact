adcc9573440ee00a236110aefaf5425b43587157
ankitxcoding <ankitxcoding@gmail.com>
2023-11-15 12:31:32 +0530

    namasteReact Episode 5

--- Episode 5/src/components/Body.js
+++ Episode 5/src/components/Body.js
@@ -1,15 +1,19 @@
 import ResList from "./ResList";
 import {resData} from "../utils/resData";
+import { useState } from "react";
 
 const Body=()=> {
+    const [listOfRes, setListOfRes]=useState(resData);
     return (
         <div className="body">
             <div className="search">
-                <input type="search" id="search" name="Search Here..." />
-                <button>Search</button>
+                <button className="btn" onClick={()=>{
+                    const topRatedRes=listOfRes.filter((res)=> res.info.avgRating>=4);
+                    setListOfRes(topRatedRes);
+                }}>Top Restaurant</button>
             </div>
             <div className="resContainer">
-                {resData.map(restaurant=><ResList key={restaurant.info.id} resDetails={restaurant}/>)}
+                {listOfRes.map(restaurant=><ResList key={restaurant.info.id} resDetails={restaurant}/>)}
             </div>
         </div>
     )
