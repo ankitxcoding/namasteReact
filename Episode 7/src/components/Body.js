89dba53dd734f6001418a1115cc015d82444b0d1
ankitxcoding <ankitxcoding@gmail.com>
2023-12-02 20:19:50 +0530

    namasteReact Episode 7

---
+++ Episode 7/src/components/Body.js
@@ -0,0 +1,45 @@
+import ResList from "./ResList";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
+const Body=()=> {
+    const [listOfRes, setListOfRes]=useState([]);
+    const [filterRes, setFilterRes]=useState([]);
+    const [searchRes, setSearchRes]=useState("");
+
+    useEffect(()=> {
+        fetchData();
+    }, []);
+
+    const fetchData=async()=> {
+        const data=await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=25.3176452&lng=82.9739144");
+        const json=await data.json();
+        setListOfRes(json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+        setFilterRes(json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    return (listOfRes.length===0)? <Shimmer /> : (
+        <div className="body">
+            <div className="btn">
+                <div className="search">
+                    <input type="text" className="searchBox" value={searchRes} onChange={(e)=> {
+                        setSearchRes(e.target.value);
+                    }}></input>
+                    <button className="searchBtn" onClick={()=> {
+                        const resultRes=listOfRes.filter((res)=> res.info.name.toLowerCase().includes(searchRes.toLowerCase()));
+                        setFilterRes(resultRes);
+                    }}>Search</button>
+                </div>
+                <button className="filterTop" onClick={()=> {
+                    const topRatedRes=listOfRes.filter((res)=> res?.info?.avgRating>=4);
+                    setFilterRes(topRatedRes);
+                }}>Top Restaurant</button>
+            </div>
+            <div className="resContainer">
+                {filterRes.map(restaurant=><ResList key={restaurant.info.id} resDetails={restaurant}/>)}
+            </div>
+        </div>
+    )
+}
+
+export default Body;
---
+++ Episode 7/src/components/Header.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import {LOGO} from "../utils/imgLink";
+
+const Header=()=> {
+    const[loginBtn, setLoginBtn]=useState("Login");
+
+    return (
+        <div className="header">
+            <div className="logo">
+                <img src={LOGO} alt="logo"/>
+            </div>
+            <div className="navbar">
+                <ul>
+                    <li>Home</li>
+                    <li>Account</li>
+                    <li>Cart</li>
+                    <button className="loginBtn" onClick={()=> {
+                        loginBtn==="Login"? setLoginBtn("Logout") : setLoginBtn("Login");
+                    }}>{loginBtn}</button>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
